e624e7cd66a47da5000b9830bf1a67e34b6f6d05
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-04-13 23:01:56 -0300

    Primeira classe de domínio

--- src/main/java/com/lucas/springionic/resources/CategoriaResource.java
+++ src/main/java/com/lucas/springionic/resources/CategoriaResource.java
@@ -1,7 +1,10 @@
 package com.lucas.springionic.resources;
+import com.lucas.springionic.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -9,6 +12,7 @@ public class CategoriaResource {
 
     @GetMapping
     public String listar(){
+        List<Categoria> lista;
         return "uaa";
     }
 }
