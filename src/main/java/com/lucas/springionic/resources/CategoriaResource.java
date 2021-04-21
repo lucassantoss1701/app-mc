a1772eab5e1399d8e688ea7a7354c8304a777448
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-04-20 23:19:37 -0300

    Post categoria

--- src/main/java/com/lucas/springionic/resources/CategoriaResource.java
+++ src/main/java/com/lucas/springionic/resources/CategoriaResource.java
@@ -3,11 +3,10 @@ import com.lucas.springionic.domain.Categoria;
 import com.lucas.springionic.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +21,14 @@ public class CategoriaResource {
     public ResponseEntity<?> listar(@PathVariable Integer id){
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
