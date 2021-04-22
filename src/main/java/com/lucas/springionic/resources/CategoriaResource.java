547c8a82c87840952518d6142abc2e44fda278c5
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-04-21 22:30:09 -0300

    Paginação com parâmetros na requisição

--- src/main/java/com/lucas/springionic/resources/CategoriaResource.java
+++ src/main/java/com/lucas/springionic/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ import com.lucas.springionic.domain.Categoria;
 import com.lucas.springionic.dto.CategoriaDTO;
 import com.lucas.springionic.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -52,6 +53,16 @@ public class CategoriaResource {
     public ResponseEntity<List<CategoriaDTO>> findAll(){
         List<Categoria> list = service.findAll();
         List<CategoriaDTO> listDTO = list.stream().map(obj ->new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value ="/page")
+    public ResponseEntity<Page> findPage(@RequestParam(value = "page", defaultValue = "0")  Integer page,
+                                         @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                         @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+                                         @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDTO = list.map(obj ->new CategoriaDTO(obj));
         return ResponseEntity.ok().body(listDTO);
     }
 }
