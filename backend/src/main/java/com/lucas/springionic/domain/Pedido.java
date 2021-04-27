347b8f4ac216f0d37aff567fadd36b7aa9168885
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-04-26 22:07:08 -0300

    To string

--- backend/src/main/java/com/lucas/springionic/domain/Pedido.java
+++ backend/src/main/java/com/lucas/springionic/domain/Pedido.java
@@ -3,10 +3,9 @@ package com.lucas.springionic.domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Entity
 public class Pedido {
@@ -111,5 +110,26 @@ public class Pedido {
         return Objects.hash(id);
     }
 
+    public String toString() {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Pedido número: ");
+        sb.append(getId());
+        sb.append(", Instante: ");
+        sb.append(sdf.format(getInstant()));
+        sb.append(", Cliente: ");
+        sb.append(getCliente().getNome());
+        sb.append(", Situação do pagamento: ");
+        sb.append(getPagamento().getEstado().getDescricao());
+        sb.append("\nDetalhes\n");
+        for (ItemPedido ip : getItens()){
+            sb.append(ip.toString());
+        }
+        sb.append("Valor total: ");
+        sb.append(nf.format(getValorTotal()));
+        sb.append("\n");
+        return sb.toString();
+    }
 
 }
