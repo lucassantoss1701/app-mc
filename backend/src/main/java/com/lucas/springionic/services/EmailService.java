cf985b04473a8fa651ff955699b7ee6f9c10da71
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-04-26 23:37:06 -0300

    Envio de email

--- backend/src/main/java/com/lucas/springionic/services/EmailService.java
+++ backend/src/main/java/com/lucas/springionic/services/EmailService.java
@@ -1,10 +1,18 @@
 package com.lucas.springionic.services;
 
+
 import com.lucas.springionic.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
+
+import javax.mail.internet.MimeMessage;
+
 
 public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
